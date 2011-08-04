784368c23e88228394fa011fac97ff40f041cdd6
Alex Barcelo <alex.barcelo@gmail.com>
2011-08-04 17:19:16 +0200

    Corrected strange folder import for test

--- testingground/testClient.py
+++ testingground/testClient.py
@@ -3,13 +3,14 @@ Created on Aug 4, 2011
 
 @author: marius
 '''
+import sys
+# hack to import client
+sys.path.append(['../'])
+import client
 
-if __name__ == '__main__':
-    # ugly hack, importing in another directory was equally ugly 
-    execfile ('../client.py')
-    
-    firstrepo = SingleRepoClient(host='localhost', port=1356,
-        username='johnsmith', key=getKeyFromPEMfile('./johnsmith.rsa'), 
+if __name__ == '__main__':    
+    firstrepo = client.SingleRepoClient(host='localhost', port=1356,
+        username='johnsmith', key=client.getKeyFromPEMfile('./johnsmith.rsa'), 
         dbfile='./clientdb.sqlite', localpath='./repoclient')
     
     print "Token received from server:", firstrepo._token
