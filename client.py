18d61485d2e49605f5adfd583bcadb19cc82ef63
Alex Barcelo <alex.barcelo@gmail.com>
2011-08-05 13:19:45 +0200

    Adding structure to client

--- client.py
+++ client.py
@@ -10,7 +10,9 @@ from base64 import b64decode
 from xmlrpclib import ServerProxy, Binary, ProtocolError
 import sqlite3
 import os.path
+from datetime import datetime
 
+import logging
 
 def getKeyFromPEM(PEMdata):
     '''
@@ -44,6 +46,22 @@ def getKeyFromPEMfile(filename):
     # File ended, not a goot header/footer
     return None
 
+class ClientError(Exception):
+    def __init__(self, retcode='-1', file=None, call=None):
+        self.retcode = retcode
+        self.file = file
+        self.call = call
+    def __str__(self):
+        str = '\nError in client communication with server\n'
+        str+= '-----------------------------------------\n'
+        str+= 'Return code: ' + self.retcode
+        if self.file:
+            str += '\nFile being edited: ' + self.file
+        if self.call:
+            str += '\nRemote call in progress: ' + self.call
+        return str
+
+
 class SingleRepoClient:
     def __init__(self, host, port, username, key, dbfile, localpath, remotepath = '.'):
         '''
@@ -55,9 +73,14 @@ class SingleRepoClient:
         @param key: RSA key used for authentication
         @param dbfile: sqlite database file to track files and revisions
         @param localpath: Local path used to synchronize
-        @param remotepath: Remote path to synchronize with (default: .). Useful
+        @param remotepath: Remote path to synchronize with (default: .) --useful
         for shared resources or multiuser servers.
         '''
+
+        ######################################################
+        ## The following two classes have been ``inspired'' ##
+        ## from xmlrpclib.py and its ServerProxy            ##
+        ######################################################
         
         # Some magic method-caller
         class _Method:
@@ -101,6 +124,10 @@ class SingleRepoClient:
         self._localpath  = localpath
         self._remotepath = remotepath
         
+        # initialize logging facility
+        self._logger = logging.getLogger('boprox-client')
+        self._logger.setLevel(logging.DEBUG)
+        
         # open database
         self._db = sqlite3.connect(dbfile,
             detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
@@ -111,7 +138,8 @@ class SingleRepoClient:
             c.execute('''create table if not exists 
                 files(
                     idfile integer primary key autoincrement,
-                    path text unique,
+                    path text,
+                    file text,
                     deleted boolean default 0,
                     lastrev integer,
                     timestamp timestamp,
@@ -127,6 +155,9 @@ class SingleRepoClient:
         anonURL = "https://%s:%s" % (host,str(port))
         self._anonConn = ServerProxy(anonURL)
         self._requestToken()
+        
+        # Prepare the ``magic'' (no precoding of remote functions, and 
+        # automatic re-ask of tokens when expired)
         self._RemoteCaller = CallProxifier()
 
     def _requestToken(self):
@@ -137,4 +168,50 @@ class SingleRepoClient:
         self._authConn = ServerProxy(self._authURL)
         
     def ping(self):
+        '''
+        Dummy function --calling the remote dummy function
+        '''
         return self._RemoteCaller.ping()
+    
+    def CheckLocalIntegrity(self):
+        '''
+        This call forces a checksum on each and every file in the local side.
+        Useful if some things have become corrupt or strange things happen with
+        modified time attribute on files.
+        
+        Use with caution: very IO (hard-disk) intensive.
+        
+        If things have changed, local database will be updated and changes will
+        be sent to the server as usual. 
+        '''
+        pass
+    
+    def CheckRepoIntegrity(self):
+        '''
+        This call walks all the files in the repository database and checks if 
+        every file is correct (from the server point of view). If there is 
+        something wrong (outdated) this function tries to update it as a normal
+        update.
+        
+        It is quite network-intensive --much more than UpdateFromServer().
+        Recommended to use it when there are problems or when the user
+        explicitly asks for it.
+        '''
+        pass
+    
+    def UpdateFromServer(self):
+        '''
+        Get all changed things from the server (last-timestamp system) and
+        update everything that is changed.
+        
+        It is safe to call this function periodically, when there are no changes
+        it is not network-intensive.
+        '''
+        pass            
+    
+    def UpdateToServer(self):
+        '''
+        This function walks the repository looking for modified files (comparing
+        its modified
+        '''
+        pass
