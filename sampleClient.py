c88bc68d557294b1057bc64039f7a07fd5b6cb43
Alex Barcelo <alex.barcelo@gmail.com>
2011-08-15 16:32:29 +0200

    Added some docs and scripts for the first release

---
+++ sampleClient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+'''
+Created on Aug 4, 2011
+
+@author: marius
+'''
+import sys
+import client
+from time import sleep
+import logging
+import os.path
+
+HOST='localhost'
+PORT=1356
+
+PASS='IChangedIt'
+
+HOME=os.path.expanduser('~')
+REPODIR=os.path.join(HOME,'boprox-repo')
+DOTBOPROX = '.boprox'
+DBFILE=os.path.join(HOME,DOTBOPROX,'clientdb.sqlite')
+HASHESDIR=os.path.join(HOME,DOTBOPROX,'hashesdir')
+
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':    
+    adminrepo = client.SingleRepoClient(host=HOST, port=PORT,
+        username='admin', permatoken=PASS, 
+        dbfile=DBFILE, localpath=REPODIR, hashesdir=HASHESDIR)
+    
+    adminrepo.UpdateToServer()
+    adminrepo.UpdateFromServer()
+    adminrepo.CheckDeletedFiles()
