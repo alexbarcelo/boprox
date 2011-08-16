0e873de732c77683f9ba1f25fcf2489ff1d410fe
Alex Barcelo <alex.barcelo@gmail.com>
2011-08-16 12:11:19 +0200

    Changed setuptools dependencies and sth more

--- boprox/setup.py
+++ boprox/setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup (
     name = "boprox_client",
     version = "0.1",
@@ -13,12 +10,23 @@ setup (
     url='http://boprox.net',
     package_dir= {'boprox': ''},
     py_modules = ['boprox.client', 'boprox.Sanitize'],
-    long_description = read('README-client'),
+    long_description = """The boprox client is used to connect to a boprox server. 
+
+It synchronizes file with the server, and can manage all the client
+actions (revision control, rollbacks, updates, adding shares...).
+
+It's not a fully-featured client for end-users, is more a API that 
+handles all filesystem and network communication and authentication
+with the server.
+"""
     license = 'GPL',
+    dependency_links = [
+        'http://sourceforge.net/projects/boprox/files/deltaindustries/'
+        ],
     install_requires = """
+        deltaindustries >= 0.1
         rsa >= 1.0.0
         pyasn1 >= 0.0.1
-        ssl
     """,
     )
 
