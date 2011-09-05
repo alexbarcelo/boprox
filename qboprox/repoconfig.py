2356ecc6247f067365817defe061b5438fdae1c1
Alex Barcelo <alex.barcelo@gmail.com>
2011-09-05 19:56:41 +0200

    Qt build update

--- qboprox/repoconfig.py
+++ qboprox/repoconfig.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'repoconfig.ui'
 #
-# Created: Wed Aug 24 16:17:10 2011
+# Created: Mon Sep  5 14:33:38 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
--- qboprox/repositories.py
+++ qboprox/repositories.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'repositories.ui'
 #
-# Created: Wed Aug 24 16:17:10 2011
+# Created: Mon Sep  5 14:33:37 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
